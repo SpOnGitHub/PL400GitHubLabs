a47b9880808ea650da5841e14c3bd5454528a60d
sangayphuntsho101 <sangayphuntshokhandu@gmail.com>
2023-09-29 09:16:03 +1000

    Added lockPermit function

--- Client Scripts/PLScritpts.js
+++ Client Scripts/PLScritpts.js
@@ -34,15 +34,56 @@ var PMS = window.PMS || {};
               formContext
                 .getAttribute("pms_newsize")
                 .setRequiredLevel("required");
-              
             } else {
               formContext.getAttribute("pms_newsize").setRequiredLevel("none");
               formContext.ui.controls.get("pms_newsize").setVisible(false);
-
             }
           },
           (error) => alert("Error " + error.message)
         );
       }
+    }),
+    (this.lockPermitRequest = function (permitId, reason) {
+      this.entity = { entityType: "pms_permit", id: permitId };
+      this.reason = reason;
+
+      this.getMetadata = function () {
+        return {
+          bounParameter: "entity",
+          paramterTypes: {
+            entity: {
+              typeName: "mscrm.pms_permit",
+              structuralProperty: 5,
+            },
+            Reason: {
+              typeName: "Edm.String",
+              structuralProperty: 1, // Primitive Type
+            },
+          },
+          operationType: 0, // action. 1 for function, 2 for CRUD
+          operationName: "pms_LockPermit",
+        };
+      };
+    }),
+    (this.lockPermit = function (primaryControl) {
+      formContext = primaryControl;
+      let permitId = formContext.data.entity
+        .getId()
+        .replace("{", "")
+        .replace("}", "");
+      let lockPermitRequest = new PMS.lockPermitRequest(permitId, "Admin Lock");
+
+      Xrm.WebApi.online.execute(lockPermitRequest).then(
+        (result) => {
+          if (result.ok) {
+            console.log(`status: ${result.status}, ${result.statusText}`);
+            formContext.ui.setFormNotification(
+              "Status " + result.status,
+              "INFORMATION"
+            );
+          }
+        },
+        (error) => console.log(error.message)
+      );
     });
 }).call(PMS);
