c1ea42d1b426d80b6dad19ea38bac30dad6f9257
sangayphuntsho101 <sangayphuntshokhandu@gmail.com>
2023-09-28 16:54:44 +1000

    Added handlePermitTypeSettings function

--- Client Scripts/PLScritpts.js
+++ Client Scripts/PLScritpts.js
@@ -2,9 +2,31 @@ var PMS = window.PMS || {};
 
 (function () {
   (this.handleOnLoad = function (executionContext) {
+    PMS.handlePermitTypeSettings(executionContext);
     console.log("On load - Permit form");
   }),
     (this.handleOnChange = function (executionContext) {
       console.log("On change - Permit type");
+    }),
+    (this.handlePermitTypeSettings = function (executionContext) {
+      let formContext = executionContext.getFormContext();
+      let permitType = formContext.getAttribute("pms_permittype").getValue();
+
+      if (permitType == null) {
+        formContext.ui.tabs.get("inspectionsTab").setVisible(false);
+        return;
+      } else {
+        let permitTypeId = permitType[0].id;
+        Xrm.WebApi.retrieveRecord("pms_permittype", permitTypeId).then(
+          (result) => {
+            if (result.pms_requireinspections) {
+              formContext.ui.tabs.get("inspectionsTab").setVisible(true);
+            } else {
+              formContext.ui.tabs.get("inspectionsTab").setVisible(true);
+            }
+          },
+          (error) => alert("Error " + error.message)
+        );
+      }
     });
 }).call(PMS);
