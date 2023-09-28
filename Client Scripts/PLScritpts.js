0d63138903b41c59fc487294d067da2cd64d34e2
sangayphuntsho101 <sangayphuntshokhandu@gmail.com>
2023-09-28 16:35:38 +1000

    let to var change

--- Client Scripts/PLScritpts.js
+++ Client Scripts/PLScritpts.js
@@ -1,13 +1,10 @@
-let PMS = window.PMS || {};
+var PMS = window.PMS || {};
 
 (function () {
-
-    this.handleOnLoad = function(executionContext){
-        console.log("On load - Permit form");
-    },
-
-    this.handleOnChange = function(executionContext){
-        console.log("On change - Permit type"); 
-    }
-
+  (this.handleOnLoad = function (executionContext) {
+    console.log("On load - Permit form");
+  }),
+    (this.handleOnChange = function (executionContext) {
+      console.log("On change - Permit type");
+    });
 }).call(PMS);
